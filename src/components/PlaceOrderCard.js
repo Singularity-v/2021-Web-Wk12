a6860300f2b89f1d174be9d8e0503a46e0862eb6
Crystal Wu <crystalwu.home@gmail.com>
2021-06-22 04:24:00 +0800

    Modal

--- src/components/PlaceOrderCard.js
+++ src/components/PlaceOrderCard.js
@@ -1,15 +1,20 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Row, Col, Button, Spin } from "antd";
 import { LoadingOutlined } from '@ant-design/icons';
 import { createOrder, resetOrder, requestOrderDetail } from "../action"
 import { StoreContext } from "../store";
+import OrderModal from "./OrderModal";
 
 export default function PlaceOrderCard() {
   const { state: { cart, orderInfo: { loading, success, order } }, dispatch } = useContext(StoreContext);
   const { cartItems } = cart;
   const history = useHistory()
   const antIcon = <LoadingOutlined style={{ fontSize: 80, color: "#FFC72D" }} spin />;
+
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const toggleModal = () => setIsModalVisible(!isModalVisible);
 
   const placeOrderHandler = () => {
     createOrder(dispatch, cart)
@@ -45,7 +50,7 @@ export default function PlaceOrderCard() {
             <Spin indicator={antIcon} className="spinner" />
           </div>
         ) : (
-          <Row gutter={[24, 24]} style={{ marginTop:"4rem" }} className="order-row">
+          <Row gutter={[24, 24]} style={{ marginTop:"4rem" }}className="order-row">
             <Col
               xs={{ span: 22, offset: 0 }}
               lg={{ span: 9, offset: 0 }}
@@ -89,63 +94,68 @@ export default function PlaceOrderCard() {
 
             </Col>
             <Col
-              xs={{ span: 22, offset: 0 }}
-              lg={{ span: 8, offset: 0 }}
-              
+               xs={{ span: 22, offset: 0 }}
+               lg={{ span: 8, offset: 0 }}
             >
-              <div className="bg-grey">
-                <h5 style={{ color: 'black', marginBottom:"2rem" }}>Check out</h5>
-                <div className="card card-body">
-                  <h6 style={{ color: '#FFC72D' }}>Shipping</h6>
-                  <div className="checkout-content">
-                    <div>Name：</div> 
-                    <div>{cart.shippingAddress.fullName}</div>
-                  </div>
-                  <div className="checkout-content"> 
-                    <div>Address： </div> 
-                    <div>{cart.shippingAddress.address},{cart.shippingAddress.city}, 
-                    {cart.shippingAddress.postalCode},{cart.shippingAddress.country}</div>
-                  </div> 
+               <div className="bg-grey">
+               <h5 style={{ color: 'black', marginBottom:"2rem" }}>Check out</h5>
+              <div className="card card-body">
+                <h6 style={{ color: '#FFC72D' }}>Shipping</h6>
+                <div className="checkout-content">
+                  <div>Name：</div> 
+                  <div>{cart.shippingAddress.fullName}</div>
                 </div>
-                <div className="card card-body">
-                  <h6 style={{ color: '#FFC72D' }}>Payment</h6>
-                  <p className="checkout-content">
-                    <div>Method：</div> {cart.paymentMethod}
-                  </p>
-                </div>
-                <div className="card card-body card-bottom3">
-                  <h6 style={{ color: '#FFC72D' }}>Order Summary</h6>
-                  <div className="row checkout-content">
-                    <div>Items：</div>
-                    <div>${cart.itemsPrice}</div>
-                  </div>
-                  <div className="checkout-content">
-                    <div>Shipping：</div>
-                    <div>${cart.shippingPrice}</div>
-                  </div>
-                  <div className="checkout-content">
-                    <div>Tax：</div>
-                    <div>${cart.taxPrice}</div>
-                  </div>
-                </div>
-                <div className="checkout-Totalcontent">
-                    <div>
-                      <strong> Total：</strong>
-                    </div>
-                    <div>
-                      <strong>${cart.totalPrice}</strong>
-                    </div>
-                  </div>
-                <Button
-                    className="placeOrder__button"
-                    block
-                    type="primary"
-                    onClick={placeOrderHandler}
-                  >
-                    Check 
-                </Button>
+                <div className="checkout-content"> 
+                  <div>Address： </div> 
+                  <div>{cart.shippingAddress.address},{cart.shippingAddress.city}, 
+                  {cart.shippingAddress.postalCode},{cart.shippingAddress.country}</div>
+                </div> 
               </div>
-               
+              <div className="card card-body">
+                <h6 style={{ color: '#FFC72D' }}>Payment</h6>
+                <p className="checkout-content">
+                  <div>Method：</div> {cart.paymentMethod}
+                </p>
+              </div>
+              <div className="card card-body card-bottom3">
+                <h6 style={{ color: '#FFC72D' }}>Order Summary</h6>
+                <div className="checkout-content">
+                  <div>Items：</div>
+                  <div>${cart.itemsPrice}</div>
+                </div>
+                <div className="checkout-content">
+                  <div>Shipping：</div>
+                  <div>${cart.shippingPrice}</div>
+                </div>
+                <div className="checkout-content">
+                  <div>Tax：</div>
+                  <div>${cart.taxPrice}</div>
+                </div>
+              </div>
+              <div className="checkout-Totalcontent">
+                  <div>
+                    <strong> Total：</strong>
+                  </div>
+                  <div>
+                    <strong>${cart.totalPrice}</strong>
+                  </div>
+                </div>
+                <div>
+              <Button
+                  className="placeOrder__button"
+                  block
+                  type="primary"
+                  //onClick={placeOrderHandler}
+                  onClick={toggleModal}
+                >
+                  Check 
+                </Button>
+                <OrderModal
+                  isModalVisible={isModalVisible}
+                  toggleModal={toggleModal}
+                />
+                </div>
+                </div>  
 
             </Col>
           </Row>
